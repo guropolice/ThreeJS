0cdcc2197d7e94e4e8a21c0ef57b7a31648ef81b
guro portector <68913251+guropolice@users.noreply.github.com>
2021-02-21 14:39:21 +0900

    update

--- Practice_threeJS/practice.js
+++ Practice_threeJS/practice.js
@@ -1,11 +1,11 @@
-import * as THREE from 'three'
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r125/build/three.module.js'
 //https://threejsfundamentals.org/threejs/resources/threejs/r125/build/three.module.js
 console.log(THREE);
 
 function main() {
     const canvas = document.querySelector('#c');
-    const renderer = new THREE.WebGLRenderer({canvas});
-    
+    const renderer = new THREE.WebGLRenderer({ canvas });
+
     const fov = 75;   //시야각 단위 도
     const aspect = 2;  // the canvas default
     const near = 0.1;
@@ -19,9 +19,9 @@ function main() {
     {
         const color = 0xFFFFFF;
         const intensity = 1;
-        const light = new THREE.DirectionalLight(color,intensity);
-        light.position.set(-1,2,4);
-        scene.add(light);    
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(-1, 2, 4);
+        scene.add(light);
     }
 
     const boxWidth = 1;
@@ -29,42 +29,42 @@ function main() {
     const boxDepth = 1;
     const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
 
-    function makeInstance(geometry,color,x){
-        const material = new THREE.MeshPhongMaterial({color});
-        
+    function makeInstance(geometry, color, x) {
+        const material = new THREE.MeshPhongMaterial({ color });
+
         const cube = new THREE.Mesh(geometry, material);
         scene.add(cube);
 
-        cube.position.x = x;  
+        cube.position.x = x;
 
         return cube;
     }
 
     const cubes = [
-        makeInstance(geometry, 0x44aa88,  0),
+        makeInstance(geometry, 0x44aa88, 0),
         makeInstance(geometry, 0x8844aa, -2),
-        makeInstance(geometry, 0xaa8844,  2),
-      ];
+        makeInstance(geometry, 0xaa8844, 2),
+    ];
 
 
     //기기의 해상도에 따라 캔버스 리사이징  
     function resizeRendererToDisplaySize(renderer) {
         const canvas = renderer.domElement;
         const pixelRatio = window.devicePixelRatio;
-        const width = canvas.clientWidth*pixelRatio | 0;     //어떤 문법?
-        const height = canvas.clientHeight*pixelRatio | 0;   //어떤 문법?
+        const width = canvas.clientWidth * pixelRatio | 0;     //어떤 문법?
+        const height = canvas.clientHeight * pixelRatio | 0;   //어떤 문법?
         const needResize = canvas.width !== width || canvas.height !== height;
-        if(needResize){
-            renderer.setSize(width,height,false);
+        if (needResize) {
+            renderer.setSize(width, height, false);
         }
         return needResize;
     }
-    
-    function render(time){
+
+    function render(time) {
         time *= 0.001;
 
-        if(resizeRendererToDisplaySize(renderer)){
-             //카메라의 aspect(비율) 속성을 canvas의 화면 크기에 맞춰야 합니다
+        if (resizeRendererToDisplaySize(renderer)) {
+            //카메라의 aspect(비율) 속성을 canvas의 화면 크기에 맞춰야 합니다
             const canvas = renderer.domElement;
             camera.aspect = canvas.clientWidth / canvas.clientHeight;
             camera.updateProjectionMatrix();
@@ -75,12 +75,12 @@ function main() {
             const rot = time * speed;
             cube.rotation.x = rot;
             cube.rotation.y = rot;
-          });
-    
-        renderer.render(scene,camera);
-    
+        });
+
+        renderer.render(scene, camera);
+
         requestAnimationFrame(render);
-    
+
     }
     requestAnimationFrame(render);
 }
